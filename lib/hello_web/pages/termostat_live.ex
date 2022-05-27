21ae494b7d0ecd976bba4864c5502255db884370
Vosmyerka <npolevtsov@gmail.com>
2022-05-27 10:10:04 +0300

    Update termostat_live.ex

--- lib/hello_web/pages/termostat_live.ex
+++ lib/hello_web/pages/termostat_live.ex
@@ -6,17 +6,23 @@ defmodule HelloWeb.ThermostatLive do
     ~H"""
     Current temperature: <%= @temperature %>
     <button phx-click="inc_temperature">+</button>
+    <button phx-click="dec_temperature">-</button>
     """
   end
 
   @spec mount(any, map, map) :: {:ok, map}
   def mount(_params, _, socket) do
-    temperature = 11
+    temperature = 0
     {:ok, assign(socket, :temperature, temperature)}
   end
   def handle_event("inc_temperature", _value, socket) do
     new_temp = socket.assigns.temperature + 1
     {:noreply, assign(socket, :temperature, new_temp)}
+  end
+
+  def handle_event("dec_temperature", _value, socket) do
+    old_temp = socket.assigns.temperature - 1
+    {:noreply, assign(socket, :temperature, old_temp)}
   end
 
 end
